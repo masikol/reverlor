336a6760cb7f1de19dac37c3143f415e13168c0d
Maksim Sikolenko <maximdeynonih@gmail.com>
2026-06-09 17:41:18 +0300

    add script test_repeat_detection/find_mock_repeats_parallel.py

---
+++ param_selection/test_repeat_detection/find_mock_repeats_parallel.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import subprocess as sp
+import multiprocessing
+
+import polars as pl
+
+from mock_repeats_settings import REPEAT_SCOUT_DIR_PATH
+
+
+PIPELINE_DATA_DIRPATH = os.path.abspath(sys.argv[1])
+REVERLOR_FIND_FPATH = os.path.abspath(sys.argv[2])
+FIND_REPEATS_OUT_ROOT_PATH = os.path.abspath(sys.argv[3])
+FINDER = sys.argv[4]
+NUM_THREADS = int(sys.argv[5]) if len(sys.argv) > 5 else 1
+
+
+def run_reverlor_find(input_fasta_fpath, finder, out_dir_path):
+    cmd = [
+        'python3', REVERLOR_FIND_FPATH,
+        '--finder', FINDER,
+        '--minimap-m', '65',
+        '--min-repeat-len', '127',
+        '--repeat-scout', REPEAT_SCOUT_DIR_PATH,
+        input_fasta_fpath,
+        out_dir_path,
+    ]
+
+    pipe = sp.Popen(cmd, text=True, stdout=sp.PIPE, stderr=sp.PIPE)
+
+    _, stderr = pipe.communicate()
+    if pipe.returncode != 0:
+        sys.stderr.write('Error running reverlor_find.py')
+        sys.stderr.write(stderr)
+        sys.exit(1)
+    # end if
+
+    return os.path.join(out_dir_path, 'repeats_final.bed')
+# end def
+
+def make_curr_outdir_path(repeat_record_id, rate=None):
+    if rate == 0.0:
+        base_name = 'default_{}'.format(repeat_record_id)
+    else:
+        base_name = 'default_{}_{:.2f}'.format(repeat_record_id, rate)
+    # end def
+    return os.path.join(
+        FIND_REPEATS_OUT_ROOT_PATH,
+        base_name
+    )
+# end def
+
+
+def process_row(row_dict):
+    try:
+        input_fasta_fpath = os.path.join(
+            chr_with_inserts_dir_path,
+            row_dict['chr_with_insert_fname']
+        )
+        out_dir_path = make_curr_outdir_path(
+            row_dict['repeat_id'],
+            row_dict['rate']
+        )
+
+        print(row_dict['repeat_id'], row_dict['rate'])
+
+        return run_reverlor_find(input_fasta_fpath, FINDER, out_dir_path)
+    except Exception as exc:
+        return f'ERROR: {row_dict.get("repeat_id", "unknown")}: {exc}'
+    # end try
+# end def
+
+
+test_combinations_fpath = os.path.join(
+    PIPELINE_DATA_DIRPATH,
+    'test_combintations.tsv'
+)
+chr_with_inserts_dir_path = os.path.join(
+    PIPELINE_DATA_DIRPATH,
+    'chr_with_inserts'
+)
+comb_df = pl.read_csv(test_combinations_fpath, separator='\t')
+
+with multiprocessing.Pool(processes=NUM_THREADS) as pool:
+    results = pool.map(process_row, comb_df.to_dicts())
+# end with
+# Reap zombies
+pool.close()
+pool.join()
+
+for res in results:
+    if isinstance(res, str) and res.startswith('ERROR:'):
+        sys.stderr.write(res + '\n')
+        sys.exit(1)
+    # end if
+# end for
+
+
+sys.exit(0)
--- param_selection/test_repeat_detection/pipeline.sh
+++ param_selection/test_repeat_detection/pipeline.sh
@@ -8,6 +8,8 @@ FINDER='repeatscout' # choice
 MASK_ALL_BUT_REPEATS=0 # choice
 # MASK_ALL_BUT_REPEATS=1 # choice
 
+N_THREADS=8
+
 WORKDIR='/mnt/data/Max/repos/reverlor'
 reverlor_find_fpath="${WORKDIR}/reverlor/reverlor_find.py"
 genome_fasta="${WORKDIR}/param_selection/data/Mycoplasma_mycoides_JCVI-syn3.0.fasta"
@@ -27,29 +29,30 @@ done
 
 cd "${pipeline_dir}"
 
-echo "$(date) -- Running extract_mock_repeats.py"
-python3 extract_mock_repeats.py \
-    "${genome_fasta}" \
-    "${mock_repeats_file}"
+# echo "$(date) -- Running extract_mock_repeats.py"
+# python3 extract_mock_repeats.py \
+#     "${genome_fasta}" \
+#     "${mock_repeats_file}"
 
-echo "$(date) -- Running mutate_mock_repeats.py"
-python3 mutate_mock_repeats.py \
-    "${mock_repeats_file}" \
-    "${pipeline_workdir}"
+# echo "$(date) -- Running mutate_mock_repeats.py"
+# python3 mutate_mock_repeats.py \
+#     "${mock_repeats_file}" \
+#     "${pipeline_workdir}"
 
-echo "$(date) -- Running insert_mock_repeats.py"
-python3 insert_mock_repeats.py \
-    "${genome_fasta}" \
-    "${mock_repeats_file}" \
-    "${pipeline_workdir}" \
-    "${MASK_ALL_BUT_REPEATS}"
+# echo "$(date) -- Running insert_mock_repeats.py"
+# python3 insert_mock_repeats.py \
+#     "${genome_fasta}" \
+#     "${mock_repeats_file}" \
+#     "${pipeline_workdir}" \
+#     "${MASK_ALL_BUT_REPEATS}"
 
 echo "$(date) -- Running find_mock_repeats.py"
-python3 find_mock_repeats.py \
+python3 find_mock_repeats_parallel.py \
     "${pipeline_workdir}" \
     "${reverlor_find_fpath}" \
     "${find_repeats_out_root}" \
-    "${FINDER}"
+    "${FINDER}" \
+    "${N_THREADS}"
 
 echo "$(date) -- Running merge_find_results.py"
 python3 merge_find_results.py \
