df2b8d4c1362841707f93ee1f258d9baea271a91
Maksim Sikolenko <maximdeynonih@gmail.com>
2026-06-25 11:31:12 +0300

    add _fix_first_bed_column to find_repeats_REPrise.py

--- reverlor/src/find_repeats_REPrise.py
+++ reverlor/src/find_repeats_REPrise.py
@@ -20,6 +20,7 @@ def find_repeats(args: FindArgs) -> str:
     sys.stderr.write('INFO: Silently merging repeats\n')
 
     raw_bed_fpath = os.path.join(tmp_dir_path, 'REPrise_result.bed')
+    _fix_first_bed_column(args, raw_bed_fpath)
     merged_bed_fpath = os.path.join(args.output_dir, 'repeats_final.bed')
     merge_features(args, raw_bed_fpath, merged_bed_fpath)
 
@@ -76,4 +77,56 @@ def _create_raw_repeat_file(args: FindArgs,
             rm_files_if_exist('{}{}'.format(out_prefix, ext))
         # end for
     # end if
+# end def
+
+
+def _fix_first_bed_column(args: FindArgs,
+                          raw_bed_fpath: str) -> None:
+
+    # collect sequence IDs from the FASTA file
+    seq_ids = []
+    with open(args.fasta_fpath, 'rt') as f:
+        for line in f:
+            if line.startswith('>'):
+                seq_id = line[1:].strip().split()[0]
+                seq_ids.append(seq_id)
+            # end if
+        # end for
+    # end with
+
+    seq_ids.sort(key=len, reverse=True)  # longer first to avoid prefix collisions
+
+    raw_bed_tmp = raw_bed_fpath + '.tmp'
+    with open(raw_bed_fpath, 'rt') as in_handle, \
+         open(raw_bed_tmp, 'wt') as out_handle:
+
+        for line in in_handle:
+            fields = line.rstrip('\n').split('\t')
+
+            seq_id = _find_matching_seq_id(fields[0], seq_ids)
+            fields[0] = seq_id
+
+            out_handle.write('\t'.join(fields) + '\n')
+        # end for
+    # end with
+
+    os.replace(raw_bed_tmp, raw_bed_fpath)
+# end def
+
+
+def _find_matching_seq_id(chrom_value: str, seq_ids: list[str]) -> str:
+    for seq_id in seq_ids:
+        if chrom_value.startswith(f'{seq_id}_'):
+            return seq_id
+        # end if
+    # end for
+    sys.stderr.write(
+        'ERROR: cannot match raw BED col1 "{}" to any input FASTA sequence ID\n'
+        .format(chrom_value)
+    )
+    sys.stderr.write(
+        'Here are the detected input FASTA sequence IDs: {}\n'
+        .format(str(seq_ids))
+    )
+    sys.exit(1)
 # end def
